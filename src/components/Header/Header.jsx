e7526779627f4570bbeae34407c617919d966c00
Rob Ralbovsky <robertralbovsky@gmail.com>
2023-05-10 17:29:30 -0600

    Created Hero and Companies Components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import './Header.css';
 const Header = () => {
   return (
     <section className="h-wrapper">
-        <div className="h-container">
+        <div className="flexCenter paddings innerWidth h-container">
+            <img src="./logo.png" alt="logo" width={100} />
 
-            <img src="logo.png" alt="logo" width={100} />
-            <div className="h-menu">
-                <a href="#">Residencies</a>
-                <a href="#">Our Value</a>
-                <a href="#">Contact Us</a>
-                <a href="#">Get Started</a>
+            <div className="flexCenter h-menu">
+                <a href="">Residencies</a>
+                <a href="">Our Value</a>
+                <a href="">Contact Us</a>
+                <a href="">Get Started</a>
                 <button className="button">
-                  <a href="#">Contact</a>
+                  <a href="">Contact</a>
                 </button>
             </div>
         </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
-
+import './Hero.css'
+import { HiLocationMarker } from 'react-icons/hi'
+import CountUp from 'react-countup'
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="hero-wrapper">
+        <div className="flexCenter paddings innerWidth hero-container">
+            
+
+            <div className="flexColStart hero-left">
+                <div className="hero-title">
+                    <div className="orange-circle" />
+                    <h1>
+                        Discover <br />
+                        More Suitable <br /> Properties
+                    </h1>
+                </div>
+                <div className="flexColStart hero-des">
+                  <span className="secondaryText">Find a variety of properties that suit your needs</span>
+                  <span className="secondaryText">Forget all the hassle of finding the perfect property</span>
+                </div>
+
+                <div className="flexCenter search-bar">
+                    <HiLocationMarker color="var(--blue)" size={25} />
+                    <input type="text" placeholder="Search Properties" />
+                    <button className="button">Search</button>
+                </div>
+
+                <div className="flexCenter stats">
+                    <div className="flexColCenter stat">
+                        <span>
+                            <CountUp start={3600} end={4250} duration={4} />
+                            <span>+</span>
+                        </span>
+                        <span className="secondaryText">
+                            Premium Listings
+                        </span>
+                    </div>
+                    <div className="flexColCenter stat">
+                        <span>
+                            <CountUp start={1200} end={1750} duration={4} />
+                            <span>+</span>
+                        </span>
+                        <span className="secondaryText">
+                            Happy Customers
+                        </span>
+                    </div>
+                    <div className="flexColCenter stat">
+                        <span>
+                            <CountUp end={17} />
+                            <span>+</span>
+                        </span>
+                        <span className="secondaryText">
+                            Awards Won
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div className="flexCenter hero-right">
+                <div className='image-container'>
+                    <img src="./hero-image.png" alt="hero" />
+                </div>
+            </div>
+        </div>    
+    </section>
   )
 }
 
